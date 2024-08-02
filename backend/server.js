37ce64a9c406c2f60a87defa6dfc422322882716
MartyPR <martin.p1131@gmail.com>
2024-08-02 14:46:45 -0500

    create folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const app = express();
+
+//!PORT
 const PORT = process.env.PORT || 5000;
 
+//! Run SERVER
 app.listen(PORT, console.log(`The server is running in ${PORT}`));
+
+
