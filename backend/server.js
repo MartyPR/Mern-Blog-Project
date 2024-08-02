6d77c3a36ad91e0e734e95885212ac5928d92d41
MartyPR <martin.p1131@gmail.com>
2024-08-02 14:38:08 -0500

    edit nodejs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`The server is running in ${PORT}`));
