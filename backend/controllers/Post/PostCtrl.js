07eadb0a69196628f491da28e57181b87db8ae99
MartyPR <martin.p1131@gmail.com>
2024-08-05 15:18:33 -0500

    create vite to fronted

--- backend/controllers/Post/PostCtrl.js
+++ backend/controllers/Post/PostCtrl.js
@@ -9,7 +9,7 @@ const CreatePost = async (req, res) => {
 
     res.json({
       status: "success",
-      message: "cPost created successfully",
+      message: "Post created successfully",
       postCreated,
     });
   } catch (error) {
@@ -18,17 +18,16 @@ const CreatePost = async (req, res) => {
 };
 //ListPost
 const listPosts = async (req, res) => {
-    try {
-      const postData = req.body;
-      const postCreated = await postModel.create(postData);
-  
-      res.json({
-        status: "success",
-        message: "cPost created successfully",
-        postCreated,
-      });
-    } catch (error) {
-      res.json(error);
-    }
-  };
-  
+  try {
+    const postData = req.body;
+    const postCreated = await postModel.create(postData);
+
+    res.json({
+      status: "success",
+      message: "cPost created successfully",
+      postCreated,
+    });
+  } catch (error) {
+    res.json(error);
+  }
+};
